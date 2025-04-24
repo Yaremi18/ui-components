2292254d4f63b228566006420b7565f97b682559
Yaremi Astorga <yare1894@gmail.com>
2025-04-24 12:18:39 -0700

    feat(components): Add button component

---
+++ src/components/button/index.tsx
@@ -0,0 +1,38 @@
+import './styles.css';
+
+interface ButtonProps {
+  /** Is this the principal call to action on the page? */
+  type?: 'primary' | 'default' | 'link';
+  /** What background color to use */
+  backgroundColor?: string;
+  /** How large should the button be? */
+  size?: 'sm' | 'md' | 'lg';
+  /** Optional click handler */
+  onClick?: () => void;
+  /** What is the content? */
+  children: React.ReactNode;
+}
+
+/** Button component for user interaction */
+export const Button = ({
+  type = 'default',
+  size = 'md',
+  backgroundColor,
+  children,
+  ...props
+}: ButtonProps) => {
+  return (
+    <button
+      type="button"
+      className={[
+        'storybook-button',
+        `storybook-button--${type}`,
+        `storybook-button--${size}`,
+      ].join(' ')}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
