2eef25ae28dc3a65fc830f4af54bfbc50e314d41
Yaremi Astorga <yare1894@gmail.com>
2025-04-24 17:45:46 -0700

    feat(stories): Add story for input component

---
+++ src/stories/Input.stories.ts
@@ -0,0 +1,66 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import { Input } from '@/components/input';
+import { ComponentProps } from 'react';
+
+type StoryProps = ComponentProps<typeof Input>;
+
+const meta: Meta<StoryProps> = {
+  title: 'Example/Input',
+  component: Input,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {
+    size: { control: { type: 'select' } },
+    type: {
+      control: { type: 'select' },
+      options: ['text', 'email', 'password', 'number'],
+    },
+  },
+};
+
+export default meta;
+
+type Story = StoryObj<StoryProps>;
+
+export const Empty: Story = {
+  args: {
+    type: 'text',
+    size: 'md',
+    placeholder: 'Write something here...',
+  },
+};
+
+export const WithDefaultValue: Story = {
+  args: {
+    type: 'text',
+    size: 'md',
+    defaultValue: 'With default value',
+  },
+};
+
+export const Small: Story = {
+  args: {
+    type: 'text',
+    size: 'sm',
+    placeholder: 'Small input',
+  },
+};
+
+export const Medium: Story = {
+  args: {
+    type: 'text',
+    size: 'md',
+    placeholder: 'Medium input',
+  },
+};
+
+export const Large: Story = {
+  args: {
+    type: 'text',
+    size: 'lg',
+    placeholder: 'Large input',
+  },
+};
